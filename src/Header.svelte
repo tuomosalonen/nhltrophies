a8848bcd031ff80f3e1283439ec2e09c500593a3
Tuomo Salonen <ab8601@student.jamk.fi>
2023-09-24 13:04:55 +0300

    initial commit

---
+++ src/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<div class="header">
+  <img class="logo" src="nhllogo.png" alt="The NHL logo" />
+</div>
+
+<style>
+  .header {
+    display: flex;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS',
+      sans-serif;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .logo {
+    max-width: 100%; /* Allow the image to scale down */
+    height: auto; /* Maintain the image's aspect ratio */
+    width: 150px; /* Set a maximum width */
+  }
+</style>
